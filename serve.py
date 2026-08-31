fa140675c77c9eb95d722b1f88b5a02c3d897e84
Jeff Henry <jbhenry@gmail.com>
2026-08-31 17:46:50 -0400

    Update waitress server startup in serve.py to add logging. Ignore log files in .gitignore

--- serve.py
+++ serve.py
@@ -1,5 +1,15 @@
 from waitress import serve
 from app import app
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(name)s: %(message)s',
+    handlers=[
+        logging.FileHandler('logs/skisale_app.log'),
+        logging.StreamHandler()  # keeps console output too
+    ]
+)
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000, threads=4)
